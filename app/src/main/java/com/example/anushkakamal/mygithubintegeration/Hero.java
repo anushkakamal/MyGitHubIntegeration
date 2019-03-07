fc8878ec8b591212b3dfed70d309078e1568ce15
anushkakamal <anushkakamal18@gmail.com>
2019-03-07 12:31:16 +0530

    first change made

---
+++ app/src/main/java/com/example/anushkakamal/mygithubintegeration/Hero.java
@@ -0,0 +1,9 @@
+package com.example.anushkakamal.mygithubintegeration;
+
+/**
+ * Created by Anushka Kamal on 3/7/2019.
+ */
+
+public class Hero {
+//    ghvghvghvhvhgv
+}
--- app/src/main/java/com/example/anushkakamal/mygithubintegeration/MainActivity.java
+++ app/src/main/java/com/example/anushkakamal/mygithubintegeration/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.anushkakamal.mygithubintegeration;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText editText;
+Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText=findViewById(R.id.edit1);
+        b1=findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=editText.getText().toString().trim();
+
+                Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    public void setdata()
+    {
+        Toast.makeText(this, "kjasdhakshdjkahdkjakjd", Toast.LENGTH_SHORT).show();
+    }
+
 }
